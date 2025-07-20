0af06d8e4d4f3fef1e485c356033bf8d0b227a41
Selamawit <selamawit8916@gmail.com>
2025-07-20 17:22:56 +0300

    Add inline CSS styles to all components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '15px', fontSize: '1.1rem', backgroundColor: '#eef', margin: '10px' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
