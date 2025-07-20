01c7b72025a41e2d55c974899a585c86bb4b2b9a
Selamawit <selamawit8916@gmail.com>
2025-07-20 19:55:06 +0300

    Add required inline styles to Navbar: backgroundColor, display, justifyContent

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ background: '#333', padding: '10px' }}>
+    <nav
+      style={{
+        backgroundColor: '#333',
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '10px',
+      }}
+    >
       <Link to="/" style={{ color: '#fff', margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ color: '#fff', margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ color: '#fff', margin: '0 10px' }}>Services</Link>
