c7429718985b8a7bfacfe5483dc1c0f99fcd8726
Selamawit <selamawit8916@gmail.com>
2025-07-26 18:17:53 +0300

    update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AddRecipeForm from './components/AddRecipeForm';
-import recipeStore from './components/recipeStore';  
+import RecipeList from './components/RecipeList'; 
 
 function App() {
   const [count, setCount] = useState(0)
