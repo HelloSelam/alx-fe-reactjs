6cca85bbaf2920c580078514aba9a357db8f878f
Selamawit <selamawit8916@gmail.com>
2025-08-02 20:05:44 +0300

    modified githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -10,7 +10,7 @@ export const advancedUserSearch = async ({ username, location, minRepos }) => {
 
   if (username) query += `${username} in:login `;
   if (location) query += `location:${location} `;
-  if (minRepos) query += `repos:>=${minRepos}`;
+  if (minRepos) query += `minRepos:>=${minRepos}`;
 
   const response = await axios.get(`https://api.github.com/search/users?q=${encodeURIComponent(query)}`);
   return response.data.items;
