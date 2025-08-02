e85497b7f766accfa067e5c4c311ebdf094e0486
Selamawit <selamawit8916@gmail.com>
2025-08-02 18:01:55 +0300

    modified githubService.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+export const fetchUserData = async (username) => {
+  const response = await axios.get(`https://api.github.com/users/${username}`);
+  return response.data;
+};
+
 export const advancedUserSearch = async ({ username, location, repos }) => {
   let query = '';
 
