6bbc1c66b47ec107ffedc8a7b567a9f4572baa08
Selamawit <selamawit8916@gmail.com>
2025-08-14 23:45:05 +0300

    feat: add responsive Home Page with mock recipe data and Tailwind styling

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,11 +1,7 @@
+import HomePage from "./components/HomePage";
+
 function App() {
-  return (
-    <div className="p-6 text-center">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Tailwind v3 is working 🎉
-      </h1>
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
