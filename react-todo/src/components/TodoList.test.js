1c74d6494251a2be95ae6d4e6ad2e4f2a45804d8
Selamawit <selamawit8916@gmail.com>
2025-08-24 18:28:00 +0300

    added a duplicated TodoList.test.js file inside src/components folder

---
+++ react-todo/src/components/TodoList.test.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build a project")).toBeInTheDocument();
+  });
+
+  test("can add a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByLabelText("todo-input");
+    const button = screen.getByText("Add");
+
+    fireEvent.change(input, { target: { value: "New Task" } });
+    fireEvent.click(button);
+
+    expect(screen.getByText("New Task")).toBeInTheDocument();
+  });
+
+  test("can toggle todo completion", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle("text-decoration: line-through");
+
+    fireEvent.click(todo);
+    expect(todo).not.toHaveStyle("text-decoration: line-through");
+  });
+
+  test("can delete a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    const deleteButton = screen.getAllByText("Delete")[0];
+
+    fireEvent.click(deleteButton);
+
+    expect(todo).not.toBeInTheDocument();
+  });
+});
